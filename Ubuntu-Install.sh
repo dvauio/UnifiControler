cb071d957e578c2c6d868856ed5f3100f0ddf82f
dvauio <davidvaughan556@gmail.com>
2019-02-11 14:42:03 +0000

    Update Ubuntu-Install.sh

--- Ubuntu-Install.sh
+++ Ubuntu-Install.sh
@@ -41,10 +41,10 @@ sudo echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
 sudo sysctl vm.vfs_cache_pressure=50
 
 # Request SSL cert
-sudo certbot certonly --standalone -d unifisvr2.ripstone.co.uk
+sudo certbot certonly --standalone -d unifisvr3.ripstone.co.uk --register-unsafely-without-email --non-interactive --agree-tos
 
 #Import Cert
-cd /etc/letsencrypt/live/unifisvr2.ripstone.co.uk/
+cd /etc/letsencrypt/live/unifisvr3.ripstone.co.uk/
 openssl pkcs12 -export -in cert.pem -inkey privkey.pem -out unifi.p12 -name unifi -CAfile fullchain.pem -caname root
 mv /var/lib/unifi/keystore /var/lib/unifi/keystore.backup
 keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore unifi.p12 -srcstoretype PKCS12 -alias unifi
