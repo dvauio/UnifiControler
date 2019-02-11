ccd7cc6da3a6257ae2996a8946dbc73f3a0220ba
dvauio <davidvaughan556@gmail.com>
2019-02-11 13:30:34 +0000

    Update Ubuntu-Install.sh

--- Ubuntu-Install.sh
+++ Ubuntu-Install.sh
@@ -1,4 +1,5 @@
-#! /bin/bash -x
+#!/bin/bash
+exec > /dev/null 2>&1
 
 # Add MongoDB List Key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
