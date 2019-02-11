de231caf99ae4940bbfebdb9e253d0c12f367c53
dvauio <davidvaughan556@gmail.com>
2019-02-11 12:48:09 +0000

    Create StartInstall.sh

---
+++ StartInstall.sh
@@ -0,0 +1,5 @@
+wget Ubuntu-Install.sh https://raw.githubusercontent.com/dvauio/UnifiControler/master/Ubuntu-Install.sh
+start sleep 10
+sudo chmod +x Ubuntu-Install.sh
+start sleep 10
+./Ubuntu-Install.sh
